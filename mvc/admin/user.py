4b0e19bd9f5933c5155370f28d4b29c2ff28f3b0
Binh Tran <tranthaibinh111@yahoo.com>
2019-03-29 00:48:00 +0700

    Change status for user of password

--- mvc/admin/user.py
+++ mvc/admin/user.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 
 class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('password', )
     list_display = ('name', 'email', 'is_superuser', 'is_staff', 'is_active', 'timezone')
     ordering = ('first_name', 'last_name', 'pk')
     search_fields = ('first_name', 'last_name')
