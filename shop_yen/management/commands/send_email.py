bafc2a65c97462c9745e1dad0403bc5f8b9c3756
Binh Tran <tranthaibinh111@yahoo.com>
2019-03-31 00:36:57 +0700

    Update template for advertisement email

--- shop_yen/management/commands/send_email.py
+++ shop_yen/management/commands/send_email.py
@@ -8,11 +8,24 @@ class Command(BaseCommand):
     help = 'Insert customer info from excel file'
 
     def handle(self, *args, **options):
-        from_email = "sport@yenvangmiennam.com"
-        to_email = "yenvangmiennam@yenvangmiennam.com"
+        from_email = "Yến Vàng Miền Name <sport@yenvangmiennam.com>"
+        to_email = "binh.tt@hdwebsoft.co"
 
-        subject = "Sending with SendGrid is Fun"
-        content = "and easy to do anywhere, even with Python"
+        subject = "[Sức Khỏe] Yến sào Minh Quang"
+        content = """
+        Mọi người có nhu cầu mua yến ủng hộ em với nhe ^_^.
+
+        🔔CƠ SỞ NUÔI VÀ CHẾ BIẾN YẾN SÀO MINH QUANG📣
+        💯Yến Sào Minh Quang đảm bảo uy tín, chất lượng và hiện nay được nhiều quí khách hàng tin dùng.
+        💯Có mã vạch quốc tế.
+        💯Có đầy đủ các loại giấy tờ về an toàn thực phẩm và giấy phép kinh doanh hợp pháp theo qui định của pháp luật Việt Nam.
+        ⭕️Quí khách hàng có nhu cầu xin liên hệ Phạm Ngân 0395735218 ⭕️
+        📬Địa chỉ:
+        🚦26/16/16 Tân Thới Nhất 2, phường Tân Thới Nhất, quận 12, tp Hồ Chí Minh.
+        🚦Giao hàng toàn quốc (Free Ship tại Tp. Hồ Chí Minh)
+        💎 YẾN SÀO MINH QUANG TRÂN TRỌNG KÍNH MỜI💎
+        #yensao #minhquang #yensaominhquang #hochiminhcity #hochiminh #baclieu #kiengiang #nestbird #health
+        """
 
         response = send_mail(
             subject=subject,
